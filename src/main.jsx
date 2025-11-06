14b7f4d9bdf8427c900e53c5aa67d598fd7e9b37
Sebastian Martinez <sebaamartinez54@gmail.com>
2025-11-06 14:53:45 -0300

    Config Base

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./styles/index.css";
+import App from "./routes/appRoutes.jsx";
+
+// strict mode puede ejecutar dos veces algunos metodos para detectar errores en el codigo.
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ src/routes/appRoutes.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default App;
+// <Route path="/product/:id" element={<Product />} />
+// <Route path="*" element={<NotFound />} />; Siempre al final para rutas no encontradas.
