6b836f39ba807a9affe85cd1b797cbd1e2abafa7
Sebastian Martinez <sebaamartinez54@gmail.com>
2025-11-17 16:41:40 -0300

    Se agrego el filtro de productos por Categoria.

---
+++ src/pages/Categorias.jsx
@@ -0,0 +1,36 @@
+import { useParams } from "react-router-dom";
+import { useFetchCategoria } from "../utils/useFetchCategoria";
+import CategoriaCard from "../components/categoriaComponent/CategoriaCard";
+
+function Categorias() {
+  const { data: categorias, loading, error } = useFetchCategoria();
+  const [productos, setProducts] = useState([]);
+
+  if (loading) return <div>Cargando categorias...</div>;
+  if (error) return <div>Error: {error}</div>;
+  if (!categorias || categorias === "")
+    return <div>No hay productos en esta categoría</div>;
+
+  return (
+    <div className="p-4 bg-gray-900 min-h-screen w-full">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {categorias.length > 0 ? (
+          //condicion verdadera
+          categorias.map((cat) => (
+            <CategoriaCard
+              key={cat.id}
+              title={cat.title}
+              picture={`http://161.35.104.211:8000${cat.picture}`}
+              description={cat.description}
+            />
+          ))
+        ) : (
+          // condicion falsa
+          <p className="text-white p-4">No hay categorias</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Categorias;
