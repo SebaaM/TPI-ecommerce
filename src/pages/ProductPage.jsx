fdc4d75deb2e11e2bf47de8e294cf2fef6347927
eliasgatica1995 <gonzaelias95@gmail.com>
2025-11-18 15:56:06 -0300

    Creado componente detalle producto con datos mock up

---
+++ src/pages/ProductPage.jsx
@@ -0,0 +1,9 @@
+import { Catalogo } from '../components/Catalogo'
+function ProductPage(){
+    return (
+             <div className="w-full min-h-screen bg-gray-800">
+                  <Catalogo apiUrl={"http://161.35.104.211:8000/products/"} apiToken={"elias"}></Catalogo>
+            </div>
+    );
+}
+export default ProductPage;
