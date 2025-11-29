f5c9cdff241c01668b229cf2cc76bb74c4a39ce2
Sebastian Martinez <sebaamartinez54@gmail.com>
2025-11-29 13:41:28 -0300

    Fix: Delete producto

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useFetchProductos } from "../utils/useFetchProductos";
 import { SearchBar } from "../components/SearchBar";
 import TablaProducto from "../components/admin/TablaProducto";
@@ -6,9 +6,18 @@ import NavBar from "../components/Navbar";
 import Footer from "../components/genericos/Footer";
 import { Link } from "react-router-dom";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export default function AdminProductos() {
   const [search, setSearch] = useState("");
   const { data: productos, loading, error } = useFetchProductos();
+  const [productosState, setProductosState] = useState([]);
+
+  useEffect(() => {
+    if (productos) {
+      setProductosState(productos);
+    }
+  }, [productos]);
 
   if (error) {
     return (
@@ -26,20 +35,26 @@ export default function AdminProductos() {
     return <p className="text-white p-4">No se encontraron productos</p>;
   }
 
-  const juegosFiltrado =
-    productos?.filter((juego) =>
+  let juegosFiltrado =
+    productosState?.filter((juego) =>
       juego.title.toLowerCase().includes(search.toLowerCase())
     ) || [];
 
   // Manejo de delete
   const handleDelete = async (id) => {
-    if (confirm("¿Seguro que quieres eliminar este producto?")) {
-      const res = await fetch(`/products/${id}`, { method: "DELETE" });
-      if (res.ok) {
-        // Filtra el producto eliminado
-        setProductos(productos.filter((p) => p.id !== id));
-      }
+    const res = await fetch(`${API_URL}/products/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer elias",
+      },
+    });
+    if (res.ok) {
+      // Filtra el producto eliminado
+      const productosActualizados = juegosFiltrado.filter(
+        (juego) => juego.id !== id
+      );
     }
+    setProductosState(productosActualizados);
   };
 
   return (
