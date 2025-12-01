9ecafa92a931f7b78209d1dcf391ecbbbf169b95
Sebastian Martinez <sebaamartinez54@gmail.com>
2025-12-01 16:12:26 -0300

    se movieron los botones del admin productos

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -4,19 +4,19 @@ import { SearchBar } from "../components/SearchBar";
 import TablaProducto from "../components/admin/TablaProducto";
 import NavBar from "../components/Navbar";
 import Footer from "../components/genericos/Footer";
-import TablaCategorias from "../components/admin/TablaCategorias";import { Link } from "react-router-dom";
+import TablaCategorias from "../components/admin/TablaCategorias";
+import { Link } from "react-router-dom";
 import LoaderAzul from "../components/genericos/LoaderAzul";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 export default function AdminProductos() {
-   //estado de barra de busqueda de categorias en la tabla
-   const [searchCat, setSearchCat] = useState("");
+  //estado de barra de busqueda de categorias en la tabla
+  const [searchCat, setSearchCat] = useState("");
 
   //estado de barra de busqueda de productos en la tabla
   const [search, setSearch] = useState("");
 
-  
   //fetch de productos
   const { data: productos, loading, error } = useFetchProductos();
   const [productosState, setProductosState] = useState([]);
@@ -27,19 +27,18 @@ export default function AdminProductos() {
     }
   }, [productos]);
 
- 
   //estado para renderizado condicional de cual crud se desea usar (entre productos y categorias)
   const [abmProductos, setAbm] = useState(true);
 
   //mostrar productos y reiniciar input de busqueda
-  function mostrarProductos(){
-    setSearch("")
-    setAbm(true) 
+  function mostrarProductos() {
+    setSearch("");
+    setAbm(true);
   }
   //mostrar categorias y reiniciar input de busqueda
-  function mostrarCategorias(){
-    setSearchCat("")
-    setAbm(false)
+  function mostrarCategorias() {
+    setSearchCat("");
+    setAbm(false);
   }
 
   if (!productos) {
@@ -69,58 +68,61 @@ export default function AdminProductos() {
   return (
     <>
       <div className="min-h-screen">
-      <NavBar />
-       {/* Si hay un error */}
-      { error && (<div>Error: {error}</div>)
-      }
-      {/* Cargando */}
-      {loading && (
-         <div  className="min-h-screen">
-            <LoaderAzul/>
-        </div>
-          
+        <NavBar />
+        {/* Si hay un error */}
+        {error && <div>Error: {error}</div>}
+        {/* Cargando */}
+        {loading && (
+          <div className="min-h-screen">
+            <LoaderAzul />
+          </div>
         )}
 
-      {!loading && (
+        {!loading && (
+          <div className="pt-20 px-10 bg-[#1b1d1f] min-h-screen text-white">
+            <div className="mt-12 md:mt-1 flex justify-between  font-bold text-white mb-4">
+              <button
+                className="mr-1 px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded shadow"
+                onClick={() => mostrarProductos()}
+              >
+                Administrar productos
+              </button>
 
-      <div className="pt-20 px-10 bg-[#1b1d1f] min-h-screen text-white">
-        <div className="mt-12 md:mt-1 flex justify-between  font-bold text-white mb-4">
-           <button
-            className="mr-1 px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded shadow"
-            onClick={() => mostrarProductos()}
-          >
-            Administrar productos
-          </button>
-          
-          <button
-            className="ml-1 px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded shadow"
-            onClick={() => mostrarCategorias()}
-          >
-            Administrar categorias
-          </button>
+              <button
+                className="ml-1 px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded shadow"
+                onClick={() => mostrarCategorias()}
+              >
+                Administrar categorias
+              </button>
+            </div>
+            {/* Listado de productos */}
+            {abmProductos && (
+              <>
+                <div className=" flex justify-between  font-bold text-white mb-4">
+                  {/* Busqueda en tabla de productos */}
+                  <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded shadow">
+                    <Link to="/admin/crearProducto">+ Agregar videojuego</Link>
+                  </button>
 
-        </div>
-        {/* Listado de productos */}
-         {abmProductos && (
-          <>
-              <div className=" flex justify-between  font-bold text-white mb-4">
-                 {/* Busqueda en tabla de productos */}
-                 <SearchBar value={search} onChange={setSearch} />
-                 <h1 className="text-2xl">Lista Productos</h1>
-                 <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded shadow">
-                  <Link to="/admin/crearProducto">+ Agregar videojuego</Link>
-                 </button>
-              </div>
-              <TablaProducto productos={juegosFiltrado} onDelete={handleDelete} />
-          </>
-          )}
+                  <h1 className="text-2xl">Lista Productos</h1>
+                  <SearchBar value={search} onChange={setSearch} />
+                </div>
+                <TablaProducto
+                  productos={juegosFiltrado}
+                  onDelete={handleDelete}
+                />
+              </>
+            )}
 
-          {/* Listado de categorias */}
-          {!abmProductos && (    
-                  <TablaCategorias searchCat={searchCat} setSearchCat={setSearchCat}/>
-          )}
-      </div>
-      )}
+            {/* Listado de categorias */}
+            {!abmProductos && (
+              <TablaCategorias
+                searchCat={searchCat}
+                setSearchCat={setSearchCat}
+              />
+            )}
+          </div>
+        )}
       </div>
       <Footer />
     </>
